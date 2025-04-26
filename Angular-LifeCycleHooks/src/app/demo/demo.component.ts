715bc71083407562d5e1ae761fd4adea727a8859
Nitika-git-design <nitikashah947@gmail.com>
2025-04-27 00:35:29 +0530

    Auto-commit on 2025-04-27 00:35:28

--- Angular-LifeCycleHooks/src/app/demo/demo.component.ts
+++ Angular-LifeCycleHooks/src/app/demo/demo.component.ts
@@ -1,24 +1,29 @@
-import { Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnChanges {
+export class DemoComponent implements OnChanges ,OnInit{
 title:string = "Example";
 @Input() message:string;
+@ViewChild('temp') tempPara:ElementRef;
 constructor(){
   console.log("Demo Component Constructor is called");
   //when is constructor is called ,by that time,none of the input properties are updated and available to use
   //hence the result of the below lines of code will not be updated.
-  console.log(this.title);
-  console.log(this.message);
+  // console.log(this.title);
+  // console.log(this.message);
 }
 ngOnChanges(changes:SimpleChanges){
   console.log("ngOnChanges is called");
-  console.log(this.message);
-  console.log(changes);
+  // console.log(this.message);
+  // console.log(changes);
+}
+ngOnInit(){
+  console.log("ngOnInit is called");
+  // console.log(this.tempPara.nativeElement); //this will return an Error as this.tempPara is undefined as at the time when ngOnInit is called the @ViewChild is not available
   
 }
 }
