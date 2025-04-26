ea654c07cb5b3e514ecf43158c98384613643db4
Nitika-git-design <nitikashah947@gmail.com>
2025-04-26 23:18:11 +0530

    Auto-commit on 2025-04-26 23:18:10

--- Angular-LifeCycleHooks/src/app/app.component.ts
+++ Angular-LifeCycleHooks/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(){
+  console.log("App Component Constructor is called");
+  }
   title = 'Angular-LifeCycleHooks';
 }
--- Angular-LifeCycleHooks/src/app/demo/demo.component.ts
+++ Angular-LifeCycleHooks/src/app/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent {
-
+title:string = "Example";
+@Input() message:string = "Hello";
+constructor(){
+  console.log("Demo Component Constructor is called");
+  //when is constructor is called ,by that time,none of the input properties are updated and available to use
+  //hence the result of the below lines of code will not be updated.
+  console.log(this.title);
+  console.log(this.message);
+}
 }
