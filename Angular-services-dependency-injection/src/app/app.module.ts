1b2a9a27a01ea0cac9f25e4dc31b957f527d0004
Nitika-git-design <nitikashah947@gmail.com>
2025-05-17 18:42:09 +0530

    Auto-commit on 2025-05-17 18:42:09

--- Angular-services-dependency-injection/src/app/app.module.ts
+++ Angular-services-dependency-injection/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './header/admin/admin.component';
+import { HomeComponent } from './header/home/home.component';
+import { SideBarComponent } from './header/home/side-bar/side-bar.component';
+import { HeroComponent } from './header/home/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AdminComponent,
+    HomeComponent,
+    SideBarComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
