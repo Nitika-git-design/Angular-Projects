e43a7e2dba0a4a5d68ce3a2332574f9d656b722e
Nitika-git-design <nitikashah947@gmail.com>
2025-06-07 22:17:43 +0530

    Auto-commit on 2025-06-07 22:17:43

--- Reactive-form/src/app/app.component.ts
+++ Reactive-form/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Reactive-form';
+  reactiveForm:FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstName :new FormControl(null,Validators.required),
+      lastName :new FormControl(null,Validators.required),
+      email :new FormControl(null,[Validators.required,Validators.email]),
+      username :new FormControl(null),
+      dob :new FormControl(null),
+      gender :new FormControl('Male'),
+      address:new FormGroup({
+      street:new FormControl(null,Validators.required),
+      city:new FormControl(null,Validators.required),
+      country :new FormControl('India',Validators.required),
+      region :new FormControl(null),
+      postal :new FormControl(null,Validators.required),
+      skills:new FormArray([
+        new FormControl(null),
+        new FormControl(null),
+        new FormControl(null)
+      ])
+       })
+    })
+  }
+  onFormSubmit(){
+    console.log(this.reactiveForm);
+    
+  }
 }
