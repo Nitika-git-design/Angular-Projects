2ed4051a18b586b4f07e798a56014ce4ef2b596d
Nitika-git-design <nitikashah947@gmail.com>
2025-06-08 21:49:59 +0530

    Auto-commit on 2025-06-08 21:49:59

--- Angular-router/src/app/home/home.component.ts
+++ Angular-router/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+activateRoute:ActivatedRoute=inject(ActivatedRoute);
+ngOnInit(){
+  this.activateRoute.fragment.subscribe((data)=>{
+    console.log(data);
+    this.jumpToSection(data);
+  })
+}
+jumpToSection(section){
+  document.getElementById(section).scrollIntoView({behavior:'smooth'})
+}
 }
